d346178fc108119adb5519df93dc4c94ed964d17
krisamorim <kris.a.furtado@gmail.com>
2020-02-15 23:01:07 -0300

    Move all files from dh repo to here

---
+++ DH/02-Cross/00025-01-Git.js
@@ -0,0 +1,31 @@
+/*
+git config --global user.name “NameYouWanna” 
+
+git config --global user.email “YourEmailAddress” 
+
+git init -> Start repository on current folder 
+
+git add example.txt -> Star track on file example.txt (change file to stage area) 
+
+git commit -m “Describe the changes made in files” -> Save de changes 
+
+git commit -a  -m “Describe changes made in files” -> Make add and commit together 
+
+git remote add origin https://github.com/krisamorim/gittest.git -> set repository to gittest.get  
+
+git push -u origin master -> sent files do repository 
+
+git status -> Show files in the stage area 
+
+git diff -> show changes waiting to be added on stage area 
+
+git diff --staged -> show files on stage area 
+
+git log -> shows lots of information including the keys of each commit  
+
+git log -p -> shows the diff of each commit 
+
+git log -p -10 -> bring just 10 commits 
+
+sudo dnf install git-gui gitk -> install interface manager log manager 
+*/
