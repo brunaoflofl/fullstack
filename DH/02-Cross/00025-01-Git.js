525300d81b63e9b0b9209edfaec229038ff06e4d
krisamorim <kris.a.furtado@gmail.com>
2020-02-17 18:55:59 -0300

    add new class

--- DH/02-Cross/00025-01-Git.js
+++ DH/02-Cross/00025-01-Git.js
@@ -1,31 +1,20 @@
 /*
-git config --global user.name “NameYouWanna” 
-
-git config --global user.email “YourEmailAddress” 
-
+git config --global user.name “NameYouWanna”  -> define user, this makes traceability easier
+git config --global user.email “YourEmailAddress” -> define email, this makes traceability easier
 git init -> Start repository on current folder 
-
+git remote add origin linkRepository -> link the current initialized forlder to a remote repository
+git remote -v -> to see link between folder and remote repository
 git add example.txt -> Star track on file example.txt (change file to stage area) 
-
 git commit -m “Describe the changes made in files” -> Save de changes 
-
 git commit -a  -m “Describe changes made in files” -> Make add and commit together 
-
 git remote add origin https://github.com/krisamorim/gittest.git -> set repository to gittest.get  
-
 git push -u origin master -> sent files do repository 
-
 git status -> Show files in the stage area 
-
 git diff -> show changes waiting to be added on stage area 
-
 git diff --staged -> show files on stage area 
-
 git log -> shows lots of information including the keys of each commit  
-
 git log -p -> shows the diff of each commit 
-
 git log -p -10 -> bring just 10 commits 
-
-sudo dnf install git-gui gitk -> install interface manager log manager 
+git clone url -> download a copy from repository
+git pull origin master -> the same as the git clone, but downloads only the changes, as an update 
 */
---
+++ DH/03-Node/00026.01.js
@@ -0,0 +1,7 @@
+/*
+npm init -y -> it creates a json file, the flag -y says yes for all questions
+porperty main in json file -> entry point entry
+npm install packageName --save -> 
+npm update -> update lybraris
+npm uninstall package -> remove a package
+*/
