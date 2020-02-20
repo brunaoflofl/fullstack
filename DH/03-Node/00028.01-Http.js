7292877bedd786cc069eb03b685713935bb9c09a
krisamorim <kris.a.furtado@gmail.com>
2020-02-20 00:32:32 -0300

    intro hhtp, modules and npm

---
+++ DH/03-Node/00028.01-Http.js
@@ -0,0 +1,24 @@
+/*
+methods Hyper text transfer protocol:
+
+get -> request data
+post -> send data (security)
+put -> send data, replace all informations from some resource
+path -> send data, modify information (partially) of a resource
+delete - > delete a resource
+
+
+codes beginning with:
+1__ -> informe 
+2__ -> Success
+3__ -> redirecting
+4__ -> client error
+5__ -> Server error
+
+200 -> ok
+307 -> temporary redirect
+403 -> forbiden
+404 -> not found
+500 -> internal server error
+
+*/
